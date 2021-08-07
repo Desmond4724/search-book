537fdd10af52a462355c459670563e27bf672fe6
Shohrux <seltashoh@gmail.com>
2021-08-07 18:20:40 +0500

    added base input

---
+++ src/components/VBtn/VBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="btn btn-primary" v-bind="$attrs">
+    <slot v-if="!loading"/>
+    <span v-else class="clock-loader"/>
+  </button>
+</template>
+
+<script>
+import "./style.scss"
+export default {
+  name: "VBtn",
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  }
+};
+</script>
